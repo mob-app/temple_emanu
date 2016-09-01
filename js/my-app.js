902f770fcaeecc83680378dc5b1ccae8425a5f95
mob-app <sahana.hyne@mrdmedia.com>
2016-09-01 16:59:12 +0530

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,88 @@
+// Let's register Template7 helper so we can pass json string in links
+Template7.registerHelper('json_stringify', function (context) {
+    return JSON.stringify(context);
+});
+
+// Initialize your app
+ var myApp = new Framework7({
+    swipeBackPage: false,
+    pushState: true,
+    
+    animateNavBackIcon: true,
+    swipePanel: 'left',
+    modalTitle: 'Title',
+});
+ 
+// Export selectors engine
+var $$ = Dom7;
+
+// Add main View
+var mainView = myApp.addView('.view-main', {
+    // Enable dynamic Navbar
+    dynamicNavbar: true,
+});
+
+
+var mySwiper = myApp.swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  paginationHide: false,
+  paginationClickable: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+});     
+
+
+
+myApp.onPageInit('sermons', function (page) {
+
+
+  var str='net';  
+         
+
+$.ajax({
+        async: true,
+        
+        type: "GET",
+        url: 'http://gicebmedia.com/sahana/temple/rss.php?q=net',
+        success: function (html) {
+            $('#output').html(html);
+            
+        },
+    });
+})
+
+
+myApp.onPageInit('shabbat', function (page) {
+
+
+  var str='heb';         
+
+$.ajax({
+        async: true,
+        
+        type: "GET",
+        url: 'http://gicebmedia.com/sahana/temple/rss.php?q=heb',
+        success: function (html) {
+            $('#output').html(html);
+            
+        },
+    });
+})
+
+
+
+
+// var myFeed = myApp.feeds('.my-feed', {
+//   url: 'http://images.apple.com/main/rss/hotnews/hotnews.rss',
+//   openIn: 'popup'
+// });
+
+
+
+
+
+
+
+
+
+         
